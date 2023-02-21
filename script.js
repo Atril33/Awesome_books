0fabe14dd5ac8c50e7306f3b4e37593634711cf5
Atril33 <118503473+Atril33@users.noreply.github.com>
2023-02-21 13:47:57 +0500

    Add new Codes

--- script.js
+++ script.js
@@ -2,12 +2,26 @@ const myTitle = document.getElementById("input");
 const myAuthor = document.getElementById("input1");
 const myAdd = document.getElementById("bttn");
 const myList = document.getElementById("list-holder");
+const Error = document.getElementById("error");
+
+function myBookStore() {
+  const todos = JSON.parse(localStorage.getItem("todos")) || [];
+  let myObject = {};
+  myObject.titel = myTitle.value;
+  myObject.autor = myAuthor.value;
+  todos.push(myObject);
+  if (myTitle.value == "" && myAuthor.value == "") {
+    todos = null;
+  }
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+myAdd.addEventListener("click", myBookStore);
 
 function myValueBook() {
-  const myTitleValue = myTitle.value;
   const myAuthorValue = myAuthor.value;
-
+  const myTitleValue = myTitle.value;
   if (myTitleValue == "" && myAuthorValue == "") {
+    Error.innerText = "Please Fill both Boxes";
   } else {
     const li = document.createElement("li");
     li.innerHTML = `${myTitleValue} <br> ${myAuthorValue} <br>`;
