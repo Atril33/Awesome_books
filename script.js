edebc47a6b4255daafef6d34988a8dd242a65e94
Peter Akhigbe <akhigbe37@gmail.com>
2023-02-22 18:46:27 +0100

    fix linter errors

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-/* eslint-disable */ 
-class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
-}
+const Error = document.getElementById('error');
+const myText = document.getElementById('text');
 
 class BookCollection {
   constructor() {
-    /* eslint-disable */
-    this.books = JSON.parse(localStorage.getItem(BOOKS_KEY)) || [];
+    this.BOOKS_KEY = 'awesome-books';
+    this.books = JSON.parse(localStorage.getItem(this.BOOKS_KEY)) || [];
     this.myTitle = document.getElementById('input');
     this.myAuthor = document.getElementById('input1');
     this.myAdd = document.getElementById('bttn');
     this.myList = document.getElementById('list-holder');
-    this.Error = document.getElementById('error');
-    this.myText = document.getElementById('text');
+  }
+
+  saveBooks() {
+    localStorage.setItem(this.BOOKS_KEY, JSON.stringify(this.books));
   }
 
   addBook() {
@@ -23,15 +20,15 @@ class BookCollection {
     const author = this.myAuthor.value.trim();
 
     if (title && author) {
-      const book = new Book(title, author);
+      const book = { title, author };
       this.books.push(book);
       this.saveBooks();
       this.renderBooks();
-      this.myTitle.value = "";
-      this.myAuthor.value = "";
-      this.Error.textContent = "";
+      this.myTitle.value = '';
+      this.myAuthor.value = '';
+      Error.textContent = '';
     } else {
-      this.Error.textContent = "Please enter both title and author";
+      Error.textContent = 'Please enter both title and author';
     }
   }
 
@@ -41,48 +38,42 @@ class BookCollection {
     this.renderBooks();
   }
 
-  saveBooks() {
-     /* eslint-disable */
-    localStorage.setItem(BOOKS_KEY, JSON.stringify(this.books));
-  }
-
   renderBooks() {
-    this.myList.innerHTML = "";
+    this.myList.innerHTML = '';
 
     if (this.books.length === 0) {
-      const emptyMessage = document.createElement("li");
-      emptyMessage.textContent = "No books in collection";
+      const emptyMessage = document.createElement('li');
+      emptyMessage.textContent = 'No books in collection';
       this.myList.appendChild(emptyMessage);
     } else {
       this.books.forEach((book) => {
-        const li = document.createElement("li");
+        const li = document.createElement('li');
         li.innerHTML = `${book.title} by ${book.author}  <br/>`;
-        li.classList = "list-items";
-        const removeBtn = document.createElement("button");
-        removeBtn.textContent = "Remove";
-        removeBtn.classList = "remove-bttn";
-        removeBtn.addEventListener("click", () => this.removeBook(book.title));
+        li.classList = 'list-items';
+        const removeBtn = document.createElement('button');
+        removeBtn.textContent = 'Remove';
+        removeBtn.classList = 'remove-bttn';
+        removeBtn.addEventListener('click', () => this.removeBook(book.title));
         li.appendChild(removeBtn);
         this.myList.appendChild(li);
-        this.myList.style.border = "2px solid #000";
-        this.myText.style.display = "block";
-        const hr = document.createElement("hr");
+        this.myList.style.border = '2px solid #000';
+        myText.style.display = 'block';
+        const hr = document.createElement('hr');
         li.appendChild(hr);
-        const myListItems = document.querySelectorAll(".list-items");
+        const myListItems = document.querySelectorAll('.list-items');
         const myLength = this.myList.childElementCount;
         for (let i = 0; i < myLength; i += 2) {
-          myListItems[i].style.backgroundColor = "#dddddd";
+          myListItems[i].style.backgroundColor = '#dddddd';
         }
       });
     }
   }
 
   init() {
-    this.myAdd.addEventListener("click", () => this.addBook());
+    this.myAdd.addEventListener('click', () => this.addBook());
     this.renderBooks();
   }
 }
 
-const BOOKS_KEY = "awesome-books";
 const bookCollection = new BookCollection();
 bookCollection.init();
